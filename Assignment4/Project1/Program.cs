2db93f57ab6885ec72cdeae55d65fe7bc57b4a60
ch f <1908445244@qq.com>
2023-03-20 19:40:43 +0800

    第四次作业

--- Assignment4/Project1/Program.cs
+++ Assignment4/Project1/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Node<T>
+{
+    public T value;
+    public Node<T> next;
+}
+
+class LinkedList<T>
+{
+    public Node<T> head;
+
+    // 添加ForEach方法
+    public void ForEach(Action<T> action)
+    {
+        // 遍历链表节点，执行action操作
+        Node<T> current = head;
+        while (current != null)
+        {
+            action(current.value);
+            current = current.next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // 创建一个泛型链表实例
+        LinkedList<int> list = new LinkedList<int>();
+        list.head = new Node<int>() { value = 10 };
+        list.head.next = new Node<int>() { value = 20 };
+        list.head.next.next = new Node<int>() { value = 30 };
+
+        // 打印链表元素
+        Console.WriteLine("The linked list values:");
+        list.ForEach(x => Console.WriteLine(x));
+
+        // 求最大值、最小值和求和
+        int max = int.MinValue;
+        int min = int.MaxValue;
+        int sum = 0;
+
+        list.ForEach(x =>
+        {
+            if (x > max) max = x; // 更新最大值
+            if (x < min) min = x; // 更新最小值
+            sum += x; // 累加求和
+        });
+
+        Console.WriteLine("The maximum value is: {0}", max);
+        Console.WriteLine("The minimum value is: {0}", min);
+        Console.WriteLine("The sum is: {0}", sum);
+    }
+}
